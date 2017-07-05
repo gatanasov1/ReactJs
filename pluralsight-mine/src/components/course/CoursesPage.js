b99a6064acb93b9fe984faaf537036c696ec7fe3
Georgi Tanev Atanasov <gatanasov@gresham-computing.com>
2017-07-05 16:39:01 +0100

    Added redux.

--- pluralsight-mine/src/components/course/CoursesPage.js
+++ pluralsight-mine/src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
 
@@ -19,14 +21,19 @@ class CoursesPage extends React.Component {
     this.setState({course: course});
   }
 
-  onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+  onClickSave = () => {
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
   }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.state.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
@@ -45,4 +52,10 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
