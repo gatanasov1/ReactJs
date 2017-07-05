4217645f66b7a9d7b9c2e907c50716373462efb2
Georgi Tanev Atanasov <gatanasov@gresham-computing.com>
2017-07-05 11:51:51 +0100

    Skeleton of the react app with routing.

---
+++ pluralsight-mine/src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>Bla bla</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+      </div>
+    );
+  }
+}
--- pluralsight-mine/tools/startMessage.js
+++ pluralsight-mine/tools/startMessage.js
@@ -1,3 +1,5 @@
 import colors from 'colors';
 
+/* eslint no-console: 0 */
+
 console.log('Starting app in dev mode...'.green);
