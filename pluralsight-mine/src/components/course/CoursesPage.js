6ebcfa527a672cf184f5b3a0a3648467f8369477
Georgi Tanev Atanasov <gatanasov@gresham-computing.com>
2017-07-06 17:05:02 +0100

    Now loading a list of courses asynchronously using redux-thunk.

--- pluralsight-mine/src/components/course/CoursesPage.js
+++ pluralsight-mine/src/components/course/CoursesPage.js
@@ -8,23 +8,6 @@ class CoursesPage extends React.Component {
 
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: '' }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const state = Object.assign({}, this.state);
-    state.course.title = event.target.value;
-    this.setState(state);
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -36,19 +19,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          value={this.state.course.title}
-          onChange={this.onTitleChange}
-        />
-
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave}
-        />
-        <p>{this.state.course.title}</p>
       </div>
     );
   }
